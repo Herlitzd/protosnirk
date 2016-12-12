d4d6309761f62ad07e1a99ed5b9346d8e72f98c1
Snirk Immington <snirk.immington@gmail.com>
2016-12-12 14:18:41 -0500

    Add verify_errors test

--- src/parse/verify/checker/mod.rs
+++ src/parse/verify/checker/mod.rs
@@ -7,6 +7,22 @@
 //! The verifiers in this module will build structures from the `build` module.
 mod symbol_checker;
 mod usage_checker;
+mod assignment_checker;
 
 pub use self::symbol_checker::SymbolTableChecker;
 pub use self::usage_checker::UsageChecker;
+pub use self::assignment_checker::AssignmentChecker;
+
+#[cfg(test)]
+mod tests {
+    use parse::verify::{ExpressionChecker, ErrorCollector, VerifyError};
+    use parse::tests::make_parser;
+
+    pub fn verify_errors<E: ExpressionChecker>(input: &'static str, mut checker: E, expected: Vec<VerifyError>) {
+        let mut parser = make_parser(input);
+        let block = parser.block().unwrap();
+        let mut verifier = ErrorCollector::new();
+        checker.check_block(&mut verifier, &block);
+        assert_eq!(verifier.get_errors(), &*expected);
+    }
+}
