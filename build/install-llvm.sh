c15e3ddc72739d622fa60138b690808b9a55bbaf
Snirk Immington <snirk.immington@gmail.com>
2017-03-27 15:12:20 -0400

    Work on CI scripts

--- build/install-llvm.sh
+++ build/install-llvm.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 set -e
 
-# Download llvm.tar.gz
-wget http://releases.llvm.org/3.7.1/clang+llvm-3.7.1-x86_64-linux-gnu-ubuntu-14.04.tar.xz -O llvm.tar.xz
+# Steal from llvm-sys build script!
+curl -L https://bitbucket.org/tari/llvm-sys.rs/downloads/llvm-3.9.1.linux.tar.xz | tar -C $HOME -xJ
+
+#VERSION="3.9.1"
+#URL=
+# Download llvm.tar.xz
+#wget http://releases.llvm.org/$VERSION/clang+llvm-$VERSION-x86_64-linux-gnu-ubuntu-16.04.tar.xz -O llvm.tar.xz
 # Extract files (no `v` arg to reduce web client spam)
-tar xfJ llvm.tar.xz
+#tar xfJ llvm.tar.xz
 # Move llvm files over
-mv -v clang+llvm-3.7.1-x86_64-linux-gnu-ubuntu-14.04 $HOME/clang
-cp -v $HOME/clang/bin/* $HOME/bin
+#mv -v clang+llvm-$VERSION-x86_64-linux-gnu-ubuntu-16.04 $HOME/llvm-install
+# Copy binaries over
+#cp -v $HOME/llvm-install/bin/* $HOME/bin
